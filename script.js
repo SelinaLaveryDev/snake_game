406687401f52d696644fe45b26a1a948f6803b5f
Selina Lavery <selina118@hotmail.com>
2025-03-25 09:02:41 +0000

    random number function

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ const score = document.querySelector(".score");
 let counter = 0;
 let moves = 100;
 let speed = 2000;
+
+// random number function
+function randomNum(num) {
+  return Math.ceil(Math.random() * num);
+}
